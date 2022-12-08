f338ad3f27fb437f202f2dd125cb2d03c3cd1512
Ipsum Coder <ipsumcoder@gmail.com>
2022-12-08 15:29:53 +0530

    Delete Index directory

--- Index/script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('div#menu-icon');
-let navbar = document.querySelector('div.menu-bar');
-
-menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navbar.classList.toggle('open-nav-bar');
-}
